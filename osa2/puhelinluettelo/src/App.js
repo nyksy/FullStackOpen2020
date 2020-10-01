a629abc66aac7782eebf11ad8a6435729061ae83
nyksy <71010049+nyksy@users.noreply.github.com>
2020-10-01 17:15:42 +0300

    osa2 tehty 2.9 asti

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,27 +1,50 @@
 import React, { useState } from 'react'
 import Name from './components/Name'
 
-//TODO ->2.7 asti
+//TODO tehty 2.9 asti
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', id: 1}])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
+
+    console.table(persons)
+
     const nameObject = {
       name: newName,
-      id: persons.length + 1,
+      number: newNumber,
     }
 
-    setPersons(persons.concat(nameObject))
+    var contains = false
+    for (let i = 0; i < persons.length; i++)
+      if (persons[i].name === newName) {
+        contains = true
+        window.alert(`${newName} is already in the phonebook`)
+      }
+    if (contains === false)
+      setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
+    contains = false
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
-  window.alert('xd')
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -34,8 +57,8 @@ const App = () => {
         </div>
         <div>
           number: <input
-          //value={}
-          //onChange={}
+          value={newNumber}
+          onChange={handleNumberChange}
           />
         </div>
         <div>
@@ -44,8 +67,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(name => 
-          <Name key={name.id} name={name} />
+        {persons.map((name, number) =>
+          <Name key={name.name} name={name} number={number} />
         )}
       </div>
     </div>
--- osa2/puhelinluettelo/src/components/Name.js
+++ osa2/puhelinluettelo/src/components/Name.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Name = ({ name }) => {
   return (
-    <p>{name.name}</p>
+    <p>{name.name} {name.number}</p>
   )
 }
 
