ce81b31cd1cf20b44c26a8847922206076aa20fd
nyksy <71010049+nyksy@users.noreply.github.com>
2020-11-21 12:16:16 +0200

    osa6 t6.9

--- osa6/anecdotes-redux/src/App.js
+++ osa6/anecdotes-redux/src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
-//TODO 6.7
+//TODO 6.9
 const App = () => {
-  
+
   return (
     <div>
       <h2>Anecdotes</h2>
